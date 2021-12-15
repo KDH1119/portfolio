24bee84c6d7fb4b445df34e887a22f460e7eaf61
KDH1119 <95996816+KDH1119@users.noreply.github.com>
2021-12-15 20:36:32 +0900

    Js; Navbar

--- main.js
+++ main.js
@@ -3,4 +3,18 @@
 // flexible === dangerous
 // added ECMAScript 5
 'use strict';
-console.log('Hello World!');
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    console.log(`scrollY: ${window.scrollY}`);
+    console.log(`navbarHeight: ${navbarHeight}`);
+
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
